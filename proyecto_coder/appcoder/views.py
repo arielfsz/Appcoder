5e15c165d4c1858280c1341750a2613f1cd3701d
Ariel Fernández <cr.arielfernandez@gmail.com>
2022-11-10 18:49:51 -0300

    FEATURE: Vistas y nuevo template

--- proyecto_coder/appcoder/views.py
+++ proyecto_coder/appcoder/views.py
@@ -1,3 +1,19 @@
+from appcoder.models import Curso
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def inicio(request):
+    return render(request,"appcoder/index.html")
+
+def cursos(request):
+    return HttpResponse("Estás en cursos")
+
+def estudiantes(request):
+    return HttpResponse("Estás en estudiantes")
+
+def profesores(request):
+    return HttpResponse("Estás en profesores")
+
+def entregables(request):
+    return HttpResponse("Estás en entregables")
--- proyecto_coder/proyecto_coder/urls.py
+++ proyecto_coder/proyecto_coder/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
+from django.urls import path, include
 from proyecto_coder.views import *
-
 
 
 urlpatterns = [
@@ -26,6 +25,7 @@ urlpatterns = [
     path('hoy/<nombre>/', dia_hoy),
     path('calcular-nacimiento/<edad>/', anio_nacimiento),
     path('inicio/', vista_plantilla),
-    path('alumnos/', vista_listado_alumnos)
+    path('alumnos/', vista_listado_alumnos),
 
+    path('coder/', include("appcoder.urls"))
 ]
